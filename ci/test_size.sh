f7ac6a5ba7f941cb36f43e938b6e002e25a6f23d
jwithers <jpwithers@gmail.com>
2021-07-05 19:21:45 -0700

    more path errors

--- ci/test_size.sh
+++ ci/test_size.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 
-make -f /opt/tensorflow/lite/micro/tools/make/Makefile clean
-make -f /opt/tensorflow/lite/micro/tools/make/Makefile third_party_downloads
+make -f /opt/tflite-micro/tensorflow/lite/micro/tools/make/Makefile clean
+make -f /opt/tflite-micro/tensorflow/lite/micro/tools/make/Makefile third_party_downloads
 
-make -f /opt/tensorflow/lite/micro/tools/make/Makefile TARGET=xtensa OPTIMIZED_KERNEL_DIR=xtensa TARGET_ARCH=fusion_f1 XTENSA_CORE=F1_190305_swupgrade keyword_benchmark BUILD_TYPE=release -j8
+make -f /opt/tflite-micro/tensorflow/lite/micro/tools/make/Makefile TARGET=xtensa OPTIMIZED_KERNEL_DIR=xtensa TARGET_ARCH=fusion_f1 XTENSA_CORE=F1_190305_swupgrade keyword_benchmark BUILD_TYPE=release -j8
 
-SIZE_LOG='/opt/tensorflow/ci/log.txt'
+SIZE_LOG='/opt/tflite-micro/ci/log.txt'
 touch ${SIZE_LOG}
-xt-size /opt/tensorflow/lite/micro/tools/make/gen/xtensa_fusion_f1_release/bin/keyword_benchmark &>> ${SIZE_LOG}
+xt-size /opt/tflite-micro/tensorflow/lite/micro/tools/make/gen/xtensa_fusion_f1_release/bin/keyword_benchmark &>> ${SIZE_LOG}
