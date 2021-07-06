6473250f2fb11b2831e74e0073acfc949eae0887
jwithers <jpwithers@gmail.com>
2021-07-06 00:31:54 -0700

    fixing head ref for main to main

--- ci/test_size.sh
+++ ci/test_size.sh
@@ -8,5 +8,6 @@ make -f /opt/tflite-micro/tensorflow/lite/micro/tools/make/Makefile third_party_
 make -f /opt/tflite-micro/tensorflow/lite/micro/tools/make/Makefile TARGET=xtensa OPTIMIZED_KERNEL_DIR=xtensa TARGET_ARCH=fusion_f1 XTENSA_CORE=F1_190305_swupgrade keyword_benchmark BUILD_TYPE=release -j8
 
 SIZE_LOG='/opt/tflite-micro/ci/log.txt'
+rm -rf ${SIZE_LOG}
 touch ${SIZE_LOG}
 xt-size /opt/tflite-micro/tensorflow/lite/micro/tools/make/gen/xtensa_fusion_f1_release/bin/keyword_benchmark &>> ${SIZE_LOG}
